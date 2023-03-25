adebefa7e905b1bca7e21592f52d39f13ad05cfe
Sebastian Herrera <sebasherrera2003@hotmail.com>
2023-03-25 18:30:54 -0500

    Web_calculator with Bootstrap and JS
    JS with arrow function and template string

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+const suma=(n1 =parseFloat(document.getElementById("n1").value) ,n2 =parseFloat(document.getElementById("n2").value) ) => respuesta.innerHTML=(`La suma es: ${n1+n2}`);
+const resta=(n1 =parseFloat(document.getElementById("n1").value) ,n2 =parseFloat(document.getElementById("n2").value) ) => respuesta.innerHTML=(`La resta es: ${n1-n2}`);
+const multi=(n1 =parseFloat(document.getElementById("n1").value) ,n2 =parseFloat(document.getElementById("n2").value) ) => respuesta.innerHTML=(`La multiplicación es: ${n1*n2}`);
+const divi=(n1 =parseFloat(document.getElementById("n1").value) ,n2 =parseFloat(document.getElementById("n2").value) ) => respuesta.innerHTML=(`La división es: ${n1/n2}`);
+
+
+
+
+
+
